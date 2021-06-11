4c853465a5df04168bc8d0a5bc39e018ac755858
Saeid Nasiri <name62777@gmail.com>
2021-06-11 21:03:36 +0430

    add javascript file

---
+++ sample.js
@@ -0,0 +1,42 @@
+//name casing
+
+//var is_valid = true
+
+var isValidUser = true
+
+var IsValidUser = true
+
+//name variable
+
+let u = {} // bad
+
+let userData , person = {} //ok
+
+let user = {} //good
+
+//name function
+
+person() //bad
+
+save() //ok
+
+saveUser() //good
+
+user.save() //good
+
+//name class
+
+// Pascal case
+
+//function parameter
+
+name() //best
+
+name('sasa') //very good
+
+name('sasa','nana') //good
+
+name('sasa','nana',6277) //avoid
+
+
+
